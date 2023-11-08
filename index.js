0a1b718cc9d0f7f3cde36c123ecbc9a566f1b116
mrlzzz <morelzzzz@gmail.com>
2023-11-08 12:52:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi!");
+});
+
+const port = process.env.port || 9001;
+app.listen(port, () => {
+    console.log("Listening at " + port);
+});
