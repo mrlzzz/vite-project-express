d415f2c40f29642f4a9be092a1f42bf098673589
mrlzzz <morelzzzz@gmail.com>
2023-11-15 21:25:26 +0100

    test

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 const app = express();
-
+const port = process.env.port || 9001;
 const corsOptions = {
     origin: "http://localhost:5173/vite-project/projects/api",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
-
 const message = {
     content: "Exemplary guestbook message",
     user: "mrl",
@@ -15,25 +14,25 @@ const message = {
 };
 
 app.use(cors());
-db.connect();
+//db.connect();
 
 app.get("/", (req, res) => {
     res.json({ test: "test" });
 });
 
 app.get("/guestbook/all", async (req, res) => {
-    try {
-        const documents = await db.findDocuments("guestbook", {});
-        console.log("Received data from the database");
-        console.log(documents);
-        res.json(documents);
-    } catch (error) {
-        console.error("Error fetching data from the database:", error);
-        res.status(500).json({ error: "Failed to fetch data" });
-    }
+    // try {
+    //     const documents = await db.findDocuments("guestbook", {});
+    //     console.log("Received data from the database");
+    //     console.log(documents);
+    //     res.json(documents);
+    // } catch (error) {
+    //     console.error("Error fetching data from the database:", error);
+    //     res.status(500).json({ error: "Failed to fetch data" });
+    // }
+    res.send(message);
 });
 
-const port = process.env.port || 9001;
 app.listen(port, () => {
     console.log("Listening at " + port);
 });
